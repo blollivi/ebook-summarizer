e3a600e073cd4816d2c690f54c98825c24dbf95c
Brendan L'OLLIVIER <blollivier@idmog.com>
2024-09-22 17:30:57 +0200

    Refactor pipeline registry to include build_summary_tree pipeline ``

--- src/book_summarizer/pipeline_registry.py
+++ src/book_summarizer/pipeline_registry.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 
-from book_summarizer.pipelines import parse_ebook, compute_embedding, summarize, populate_database
+from book_summarizer.pipelines import parse_ebook, compute_embedding, summarize, populate_database, build_summary_tree
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -15,6 +15,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     parse_ebook_pipeline = parse_ebook.create_pipeline()
     compute_embedding_pipeline = compute_embedding.create_pipeline()
+    build_summary_tree_pipeline = build_summary_tree.create_pipeline()
     summarize_all_pipeline, summarize_tree_pipeline = summarize.create_pipelines()
     populate_database_pipeline = populate_database.create_pipeline()
 
@@ -24,6 +25,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "__default__": complete_pipeline,
         "parse_ebook": parse_ebook_pipeline,
         "compute_embedding": compute_embedding_pipeline,
+        "build_summary_tree": build_summary_tree_pipeline,
         "summarize_all": summarize_all_pipeline,
         "summarize_tree": summarize_tree_pipeline,
         "populate_database": populate_database_pipeline,
--- src/book_summarizer/pipelines/build_summary_tree/pipeline.py
+++ src/book_summarizer/pipelines/build_summary_tree/pipeline.py
@@ -6,6 +6,7 @@ generated using Kedro 0.19.6
 from kedro.pipeline import Pipeline, pipeline, node
 
 from .nodes import (
+    detect_change_points,
     build_summary_tree,
     extract_tree_cut
 )
@@ -15,8 +16,14 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=build_summary_tree,
+                func=detect_change_points,
                 inputs=["umap_projection", "params:change_point_detection"],
+                outputs=["bkpts_matrix", "bkpts", "penalties"],
+                name="detect_change_points",
+            ),
+            node(
+                func=build_summary_tree,
+                inputs=["bkpts_matrix", "bkpts", "penalties"],
                 outputs="summary_tree",
                 name="build_summary_tree",
             ),
